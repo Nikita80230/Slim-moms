9fc04565dc74a730eacf230f70ac4c40a747e779
Nikita_Beliy <beliynikita1995@gmail.com>
2024-02-17 23:37:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { defineConfig } from "vite";
 import viteSvgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPathsPlugin from "vite-tsconfig-paths";
 
@@ -15,5 +14,8 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+  },
+  build: {
+    outDir: "build",
   },
 });
